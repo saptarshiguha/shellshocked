ffac35623ef9a308a59e2c78aa0066b2760aff9f
Saptarshi Guha <saptarshi.guha+github@gmail.com>
2016-02-18 13:24:29 -0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,2 +1,10 @@
 cd ~/
-git clone https://github.com/saptarshiguha/terrific  
+IS_MASTER=false
+if [ -f /mnt/var/lib/info/instance.json ]
+then
+	IS_MASTER=$(jq .isMaster /mnt/var/lib/info/instance.json)
+fi
+if $IS_MASTER; then
+  ## only runs on master
+  git clone https://github.com/saptarshiguha/terrific  
+fi
